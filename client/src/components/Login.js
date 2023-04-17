8ba0ffefd99fe13e411f8ccd79be86c1355fa724
Dezarea Bryan <dezareabryan@gmail.com>
2023-04-17 16:50:57 -0400

    Disabling navigation to home page if credentials are found. Still have a bug with updating the user.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import {
   MDBContainer,
   MDBCol,
@@ -10,50 +10,52 @@ import {
 import alterID from "../actions/alterID";
 import { useDispatch } from "react-redux";
 
-
-
 import "../styles/Login.css";
 
 const Login = () => {
-
   const dispatch = useDispatch();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleClick = () => {
-    navigate('/register')
-  }
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('')
-  const [isError, setIsError] = useState(false)
+    navigate("/register");
+  };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isError, setIsError] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const result = await fetch('https://page-by-page.onrender.com/api/login', {
-        method: 'POST',
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Headers": "*"
-        },
-        body: JSON.stringify({
-          email: email,
-          password: password
-        }),
+    const result = await fetch("https://page-by-page.onrender.com/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Headers": "*",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
     });
-    await result.json()
-    .then((data) => {
-      console.log(data)
-      dispatch(alterID(data.id))
-      console.log(data.id)
-                setEmail("");
-                setPassword("");
-                navigate('/home')
-            })
-            .catch((error) => {
-              console.log(error)
-                setIsError(true)
-                return
-            });
-    }
+    await result
+      .json()
+      .then((data) => {
+        if (data !== "Unable to login.") {
+          dispatch(alterID(data.id));
+          console.log(data.id);
+          setEmail("");
+          setPassword("");
+          navigate("/home");
+        } else {
+          console.log(data);
+        }
+      })
+
+      .catch((error) => {
+        console.log(error);
+        setIsError(true);
+        return;
+      });
+  };
   return (
     <MDBContainer fluid className="p-3 my-5">
       <MDBRow>
@@ -69,34 +71,31 @@ const Login = () => {
           <h1 id="h1-pbp">Page by Page</h1>
           <br />
           <form onSubmit={handleSubmit}>
+            <MDBInput
+              onChange={(event) => setEmail(event.target.value)}
+              wrapperClass="mb-4"
+              contrast
+              label="Email"
+              id="formControlLg"
+              type="email"
+              size="lg"
+            />
+            <MDBInput
+              onChange={(event) => setPassword(event.target.value)}
+              wrapperClass="mb-4"
+              contrast
+              label="password"
+              id="formControlLg"
+              type="password"
+              size="lg"
+            />
 
-
-          <MDBInput
-          onChange={(event) => setEmail(event.target.value)}
-          wrapperClass="mb-4"
-          contrast
-          label="Email"
-          id="formControlLg"
-          type="email"
-          size="lg"
-          />
-          <MDBInput
-          onChange={(event) => setPassword(event.target.value)}
-          wrapperClass="mb-4"
-          contrast
-          label="password"
-          id="formControlLg"
-          type="password"
-          size="lg"
-          />
-
-          <MDBBtn type='submit' className="mb-4 w-100" size="lg">
-            Sign in
-          </MDBBtn>
-
+            <MDBBtn type="submit" className="mb-4 w-100" size="lg">
+              Sign in
+            </MDBBtn>
           </form>
-          {isError ? <h4>Unable to login.</h4> : null} 
-          <p id='already-have-account'>Don't have an account?</p>
+          {isError ? <h4>Unable to login.</h4> : null}
+          <p id="already-have-account">Don't have an account?</p>
 
           <MDBBtn onClick={handleClick} className="mb-4 w-100" size="lg">
             Create Account
